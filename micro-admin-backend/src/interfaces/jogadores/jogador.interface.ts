cfd45296d4ff297f175ef3c3ca6948f76d333d69
Luis2223 <luisfsdsantiago@gmail.com>
2021-06-16 11:49:26 -0300

    add microservice

---
+++ micro-admin-backend/src/interfaces/jogadores/jogador.interface.ts
@@ -0,0 +1,9 @@
+import { Document } from 'mongoose';
+export interface Jogador extends Document {
+  readonly phoneNumber: string;
+  readonly email: string;
+  name: string;
+  ranking: string;
+  rankingPosition: number;
+  urlPictureFriend: string;
+}
